499062ceadd7f52377ac1938e15b71cf54f657e1
Sachin Tandale <sachintandale45@gmail.com>
2025-11-20 12:36:34 +0530

    Initial commit

---
+++ build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/.qt/apptrial1_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "apptrial1")
+set(working_dir "C:/NMSPL/qt_workspace/trial1")
+set(src_and_dest_list
+
+)
+set(timestamp_file "C:/NMSPL/qt_workspace/trial1/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/.qt/apptrial1_res.txt")
